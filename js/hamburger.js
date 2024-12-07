b1c60a6961182d119ddb4c7621d8cc8310803778
Ephraim Djeket <ephdje50146@stud.noroff.no>
2024-12-07 12:45:19 +0100

    Added dynamic closure of menu for larger screen

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,6 +1,16 @@
 const hamburgerEl = document.getElementById("hamburger");
 const menuEl = document.getElementById("menu");
 
+
+function hideMenuDisplay() {
+    if(window.innerWidth > 640) {
+        menuEl.classList.add("hidden");
+    } 
+};
+
+hideMenuDisplay();
+window.addEventListener("resize", hideMenuDisplay);
+
 hamburgerEl.addEventListener("click", () => {
     menuEl.classList.toggle("hidden")
 })
