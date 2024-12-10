5e418d3f247719e427bd3db5b50a446e57e5a706
Ephraim Djeket <ephdje50146@stud.noroff.no>
2024-12-10 16:44:22 +0100

    Adjustments to the user register code

--- js/auth/register.mjs
+++ js/auth/register.mjs
@@ -7,24 +7,34 @@ const registerPasswordInput = document.getElementById("register-password");
 const registerAvatarUrl = document.getElementById("register-avatarurl");
 const registerFormEl = document.getElementById("register-form");
 
+
 async function registerFetch() {
+    const bodyData = {           
+        name: registerNameInput.value,
+        email: registerEmailInput.value,
+        password: registerPasswordInput.value,
+    }
+    if(registerAvatarUrl.value !== "") {
+        bodyData.avatar = {url: registerAvatarUrl.value};
+    }
     try{
     const response = await fetch(`${API_BASE_URL}/auth/register`, {
         method: "POST",
         headers: {
             "Content-Type":"application/json",
         },
-        body: JSON.stringify({
-            name: registerNameInput.value,
-            email: registerEmailInput.value,
-            password: registerPasswordInput.value,
-            avatar: registerAvatarUrl.value,
-        }),
+        body: JSON.stringify(bodyData),
+
     });
     const json = await response.json();
-    console.log(json)
+    if (!response.ok) {
+        console.log("Error:", json.errors[0].message);
+        return;
+    } else {
+        console.log("yes")
+    }
 } catch(error) {
-    console.log("yes", response.error[0].message);
+    console.error(error.message);
 }
 }
 
