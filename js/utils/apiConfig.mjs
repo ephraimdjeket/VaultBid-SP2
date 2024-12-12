cad9cf9e452f1ac9befa89e6dd6c2e50ebb0c78a
Ephraim Djeket <ephdje50146@stud.noroff.no>
2024-12-12 17:18:17 +0100

    Added more API path variables

--- js/utils/apiConfig.mjs
+++ js/utils/apiConfig.mjs
@@ -1,1 +1,5 @@
 export const API_BASE_URL = "https://v2.api.noroff.dev";
+export const API_AUTH = "/auth";
+export const API_LOGIN = "/login";
+export const API_REGISTER = "/register";
+export const API_AUCTION_LISTINGS = "/aution/listings"
