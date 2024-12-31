5ddb954529978fbe59ccb3279e453b757262ff23
Ephraim Djeket <ephdje50146@stud.noroff.no>
2024-12-31 11:43:04 +0100

    Refractored to localStorage.clear()

--- js/auth/logout.js
+++ js/auth/logout.js
@@ -2,8 +2,7 @@ const logoutBtns = document.querySelectorAll(".logout");
 
 logoutBtns.forEach((logoutBtn) => {
     logoutBtn.addEventListener("click", () => {
-        localStorage.removeItem("name");
-        localStorage.removeItem("accessToken");
+        localStorage.clear();
     }
     )
 })
