f9ad0c1b6a6b380fe2b086c18672e4527d495bda
Ephraim Djeket <ephdje50146@stud.noroff.no>
2025-01-03 17:39:55 +0100

    Timeout on errorhandling

--- js/ui/errorHandler.mjs
+++ js/ui/errorHandler.mjs
@@ -1,6 +1,9 @@
 const errorMessageContainer = document.getElementById("errorMessage");
 
 export function displayError(message) {
-    errorMessageContainer.classList.add("flex", "justify-around", "text-center", "text-red-500", "py-5")
     errorMessageContainer.innerText = message;
+    errorMessageContainer.classList.remove("hidden");
+    setTimeout(() => {
+        errorMessageContainer.classList.add("hidden");
+    }, 3000)
 }
