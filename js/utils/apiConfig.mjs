eda406a006ba1110366fca210ac7b157e88e7e48
Ephraim Djeket <ephdje50146@stud.noroff.no>
2025-01-04 16:53:20 +0100

    Created an additional api constant

--- js/utils/apiConfig.mjs
+++ js/utils/apiConfig.mjs
@@ -3,3 +3,4 @@ export const API_AUTH = "/auth";
 export const API_LOGIN = "/login";
 export const API_REGISTER = "/register";
 export const API_AUCTION_LISTINGS = "/auction/listings"
+export const API_AUCTION_PROFILE = "/auction/profiles/";
