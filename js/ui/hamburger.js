b0b85aa7ad92b96cfeadc2d0209cc0fb869aa560
Ephraim Djeket <ephdje50146@stud.noroff.no>
2025-01-04 16:52:25 +0100

    Improved opening/closing of hamburger

--- js/ui/hamburger.js
+++ js/ui/hamburger.js
@@ -1,16 +1,28 @@
 const hamburgerEl = document.getElementById("hamburger");
+const closeEl = document.getElementById("close");
 const menuEl = document.getElementById("menu");
 
+menuEl.classList.add("hidden");
 
 function hideMenuDisplay() {
-    if(window.innerWidth > 640) {
+    if (window.innerWidth > 640) {
         menuEl.classList.add("hidden");
-    } 
+        closeEl.classList.add("hidden")
+        hamburgerEl.classList.remove("hidden")
+    }
 };
 
 hideMenuDisplay();
 window.addEventListener("resize", hideMenuDisplay);
 
 hamburgerEl.addEventListener("click", () => {
-    menuEl.classList.toggle("hidden")
+    hamburgerEl.classList.add("hidden")
+    menuEl.classList.remove("hidden")
+    closeEl.classList.remove("hidden")
+})
+
+closeEl.addEventListener("click", () => {
+    hamburgerEl.classList.remove("hidden")
+    closeEl.classList.add("hidden")
+    menuEl.classList.add("hidden")
 })
