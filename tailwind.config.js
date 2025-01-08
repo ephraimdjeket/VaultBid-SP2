1f09e5510f86d25300d2606f3f6f2dc798277a15
Ephraim Djeket <ephdje50146@stud.noroff.no>
2025-01-08 22:16:10 +0100

    Added more custom classes and safelist class

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./**/*.html', './js/**/*.js'],
+  safelist: [
+    "max-w-cards-250",
+  ],
+  important: true,
   theme: {
     extend: {
       colors: {
-        "charcoal": "#333333",
+        charcoal: "#333333",
         "golden-yellow": "#FFBF00",
         "slate-blue": "#6A5ACD",
       },
-
       fontFamily: {
-        "roboto": ['Roboto', 'sans-serif'],
-        'open-sans': ['Open Sans', 'sans-serif']
-      }
+        roboto: ['Roboto', 'sans-serif'],
+        'open-sans': ['Open Sans', 'sans-serif'],
+      },
+      maxWidth: {
+        "screen-1200": "1200px",
+        "screen-850": "850px",
+        "cards-250": "250px",
+      },
     },
-
-    maxWidth: {
-      "screen-1200": "1200px",
-      "screen-850": "850px",
-
-    }
   },
   plugins: [],
-}
+};
+
 
