388a3845985173b86da7dca91829b3c367b7c77f
Ephraim Djeket <ephdje50146@stud.noroff.no>
2025-01-09 20:18:25 +0100

    Login validation and importing functions

--- js/auction/userProfile.mjs
+++ js/auction/userProfile.mjs
@@ -1,4 +1,14 @@
 import { singleProfile, fetchListings, fetchWins } from "../ui/profileListings.mjs";
+import { userLoggedIn, isLoggedIn, isNotLoggedIn } from "../utils/userLoggedIn.mjs";
+
+if (userLoggedIn) {
+    isLoggedIn();
+    fetchListings();
+    fetchWins();
+    singleProfile();
+} else {
+    isNotLoggedIn();
+}
 
 const userName = localStorage.getItem("user");
 const userProfileName = document.getElementById("username");
@@ -9,7 +19,3 @@ const user = JSON.parse(userName);
 userProfileName.innerText = user.name;
 userProfileBio.innerText = user.bio || "How about adding a bio? 😎";
 userProfilePicture.src = user.avatar.url || "/images/placeholder-image.png";
-
-fetchListings();
-fetchWins();
-singleProfile();
