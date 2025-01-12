c8e152adb5819c7b596679ad907f6acd79546ea8
Ephraim Djeket <ephdje50146@stud.noroff.no>
2025-01-12 16:25:55 +0100

    Exporting API endpoint with query parameters

--- js/utils/apiConfig.mjs
+++ js/utils/apiConfig.mjs
@@ -4,3 +4,7 @@ export const API_LOGIN = "/login";
 export const API_REGISTER = "/register";
 export const API_AUCTION_LISTINGS = "/auction/listings"
 export const API_AUCTION_PROFILE = "/auction/profiles/";
+
+export function apiFetch(sortOrder) {
+    return `${API_BASE_URL}${API_AUCTION_LISTINGS}?_seller=true&_bids=true&sort=created&sortOrder=${sortOrder}&limit=100`
+};
