792fc2a96b038cf39ab1be858ae2adbe9e80b527
Ephraim Djeket <ephdje50146@stud.noroff.no>
2025-01-12 16:17:12 +0100

    Updated innerText of user bio

--- js/auction/userProfile.mjs
+++ js/auction/userProfile.mjs
@@ -17,5 +17,5 @@ const userProfilePicture = document.getElementById("profileImg");
 const user = JSON.parse(userName);
 
 userProfileName.innerText = user.name;
-userProfileBio.innerText = user.bio || "How about adding a bio? 😎";
+userProfileBio.innerText = user.bio || "A bio hasn't been added.";
 userProfilePicture.src = user.avatar.url || "/images/placeholder-image.png";
