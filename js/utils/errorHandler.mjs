90505d8823a68463041ecbc543ac28c3241e17e0
Ephraim Djeket <ephdje50146@stud.noroff.no>
2025-01-12 16:26:14 +0100

    Added error handling for create modal

--- js/utils/errorHandler.mjs
+++ js/utils/errorHandler.mjs
@@ -1,5 +1,6 @@
 const errorMessageContainer = document.getElementById("error-message");
 const errorMessageAvatar = document.getElementById("error-message-avatar");
+const errorMessageCreate = document.getElementById("error-message-create");
 
 export function displayError(message) {
     errorMessageContainer.innerText = message;
@@ -14,5 +15,13 @@ export function displayErrorAvatar(message) {
     errorMessageAvatar.classList.remove("hidden");
     setTimeout(() => {
         errorMessageAvatar.classList.add("hidden");
+    }, 3000)
+}
+
+export function displayErrorCreate(message) {
+    errorMessageCreate.innerText = message;
+    errorMessageCreate.classList.remove("hidden");
+    setTimeout(() => {
+        errorMessageCreate.classList.add("hidden");
     }, 3000)
 }
