4a4f9ebdb7b8e8fd15e0cd6c88f1ac9c80f4b627
Ephraim Djeket <ephdje50146@stud.noroff.no>
2025-01-16 20:30:43 +0100

    Adjusted the position of code

--- js/utils/userLoggedIn.mjs
+++ js/utils/userLoggedIn.mjs
@@ -1,3 +1,9 @@
+import { fetchListings, fetchWins } from "../ui/profileListings.mjs";
+import { singleProfile } from "../ui/profileListings.mjs";
+
+export const bidSection = document.querySelector(".bid-section");
+export const visitorMessage = document.querySelector(".guest-message");
+
 const creditDesktop = document.querySelector(".credits-desktop");
 const profileTag = document.querySelector(".profile-desktop");
 const logoutBtn = document.querySelector(".logout");
@@ -8,8 +14,6 @@ const profileMobileTag = document.querySelector(".profile-mob");
 const logoutMobileBtn = document.querySelector(".logout-mob");
 const loginMobileTag = document.querySelector(".login-mobile");
 const registerMobileTag = document.querySelector(".register-mobile");
-export const bidSection = document.querySelector(".bid-section");
-export const visitorMessage = document.querySelector(".guest-message");
 
 
 export const userLoggedIn = localStorage.getItem("user");
@@ -38,4 +42,30 @@ export function isLoggedIn() {
     logoutMobileBtn.classList.remove("hidden");
     loginMobileTag.classList.add("hidden");
     registerMobileTag.classList.add("hidden");
+}
+
+export function userLoginCheck() {
+    if (userLoggedIn) {
+        isLoggedIn();
+        singleProfile();
+
+        const currentUrl2 = window.location.href;
+        if (!currentUrl2.includes("/listing-details") && currentUrl2.includes("/profile")) {
+            fetchListings();
+        }
+        fetchWins();
+        const currentUrl3 = window.location.href;
+        if (currentUrl3.includes("/listing-details")) {
+            bidSection.classList.remove("hidden");
+        }
+
+    } else {
+        isNotLoggedIn();
+        const currentUrl1 = window.location.href;
+        if (currentUrl1.includes("/listing-details")) {
+            bidSection.classList.add("hidden");
+            visitorMessage.classList.remove("hidden");
+            visitorMessage.innerText = "Login to bid 😊";
+        }
+    }
 }
