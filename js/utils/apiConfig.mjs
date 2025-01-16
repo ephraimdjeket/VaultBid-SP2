2d9ed8b103162e7c666bef753d7ec2b7dd7fae25
Ephraim Djeket <ephdje50146@stud.noroff.no>
2025-01-16 20:30:02 +0100

    Added more URL endpoints

--- js/utils/apiConfig.mjs
+++ js/utils/apiConfig.mjs
@@ -1,10 +1,17 @@
+// Base URL
 export const API_BASE_URL = "https://v2.api.noroff.dev";
-export const API_AUTH = "/auth";
-export const API_LOGIN = "/login";
-export const API_REGISTER = "/register";
-export const API_AUCTION_LISTINGS = "/auction/listings"
-export const API_AUCTION_PROFILE = "/auction/profiles/";
+
+// Endpoints
+export const API_AUTH = `${API_BASE_URL}/auth`;
+export const API_LOGIN = `${API_AUTH}/login`;
+export const API_REGISTER = `${API_AUTH}/register`;
+export const API_AUCTION_LISTINGS = `${API_BASE_URL}/auction/listings`;
+export const API_AUCTION_PROFILE = `${API_BASE_URL}/auction/profiles`;
+
+// Helper to get dynamic URLs
+export const getAuctionListingBidsUrl = (id) => `${API_AUCTION_LISTINGS}/${id}/bids`;
+export const getAuctionProfileUrl = (profileId) => `${API_AUCTION_PROFILE}/${profileId}`;
 
 export function apiFetch(sortOrder) {
-    return `${API_BASE_URL}${API_AUCTION_LISTINGS}?_seller=true&_bids=true&sort=created&sortOrder=${sortOrder}&limit=100`
+    return `${API_AUCTION_LISTINGS}?_seller=true&_bids=true&sort=created&sortOrder=${sortOrder}&limit=100`
 };
